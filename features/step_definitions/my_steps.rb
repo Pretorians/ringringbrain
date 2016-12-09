01119a716ac49b3e45b22bb8f1d67d722ade6d23
Manuel Castillo Fresco <manuelcastillofresco@gmail.com>
2016-12-09 15:43:32 +0000

    Feature Resultado

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,7 +2,6 @@ Given(/^abro la aplicación$/) do
   visit '/'
 end
 
-<<<<<<< HEAD
 Given(/^Abrir pagina$/) do
     visit '/'
 end
@@ -13,13 +12,4 @@ end
 
 When(/^Hacer clic en el boton Start$/) do
    click_button(start)
-  end
-=======
-When(/^inicio juego$/) do
-  click_button("inicio")
 end
-
-Then(/^veo "([^"]*)"$/) do |texto|
-  expect(page.body).to match /#{texto}/m
-end
->>>>>>> d92d2ebb0cd95f8ac23a51ffc4e1305af99cdbc7
